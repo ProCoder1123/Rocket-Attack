7b0e9f42461c92d64e39b7b1aa43bc4776459808
ProCoder1123 <97404920+ProCoder1123@users.noreply.github.com>
2022-03-13 20:56:23 +0200

    Add files via upload

---
+++ game.js
@@ -0,0 +1,47 @@
+// Suzdavame promenlivi
+let myX, myY, rocketX=[], rocketY=[], ip;
+function init() {
+    // Kodut tuk se izpulnqva vednuj v nachaloto
+    myX = 300;
+    myY = 300;
+
+    rocketX[0] = 0;
+    rocketY[0] = 100;
+    rocketX[1] = 0;
+    rocketY[1] = randomInteger(600);
+    rocketX[2] = 0;
+    rocketY[2] = randomInteger(600);
+}
+function update() {
+    // Kodut tuk se izpulnqva (okolo) 100 puti v sekunda
+    myX = myX + (mouseX - myX) / 10;
+    myY = myY + (mouseY - myY) / 10;
+
+    for(ip=0; ip<3; ip=ip + 1) {
+        rocketX[ip] = rocketX[ip] + 2;
+    }
+    for(ip=0; ip<3; ip=ip + 1) {
+        // nothing here for now
+
+    }
+    
+}
+function draw() {
+    // Tuk naprogramirai kakvo da se risuva
+    drawImage(backField, 0, 0, 800, 600);
+    drawImage(femaleAction, myX, myY, 60, 80);
+    for(ip=0; ip<3; ip=ip+1){
+        drawImage(rocket[0], rocketX[ip], rocketY[ip], 35, 60);
+    }
+}
+function mouseup() {
+    // Pri klik s lqv buton - pokaji koordinatite na mishkata
+    console.log("Mouse clicked at", mouseX, mouseY);
+}
+function keyup(key) {
+    // Pechatai koda na natisnatiq klavish
+    console.log("Pressed", key);
+    
+}
+
+

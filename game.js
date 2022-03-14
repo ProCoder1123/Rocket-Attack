6a509bbee720c06126ef65cf8b158c7d455f7e04
ProCoder1123 <97404920+ProCoder1123@users.noreply.github.com>
2022-03-14 10:08:05 +0200

    v1.02

--- game.js
+++ game.js
@@ -1,26 +1,32 @@
 // Suzdavame promenlivi
-let myX, myY, rocketX=[], rocketY=[], ip;
+let myX, myY, rocketX=[], rocketY=[];
 function init() {
     // Kodut tuk se izpulnqva vednuj v nachaloto
     myX = 300;
     myY = 300;
-
+    for(var ip=0; ip<10; ip=ip + 1) {
+        if(randomInteger(10)<9){
+  /*
     rocketX[0] = 0;
     rocketY[0] = 100;
     rocketX[1] = 0;
     rocketY[1] = randomInteger(600);
     rocketX[2] = 0;
-    rocketY[2] = randomInteger(600);
+    rocketY[2] = randomInteger(600);*/
+    rocketY[ip] = ip*50;
+    rocketX[ip]=0;
+        }
+    }
 }
 function update() {
     // Kodut tuk se izpulnqva (okolo) 100 puti v sekunda
     myX = myX + (mouseX - myX) / 10;
     myY = myY + (mouseY - myY) / 10;
 
-    for(ip=0; ip<3; ip=ip + 1) {
+    for(var ip=0; ip<10; ip=ip + 1) {
         rocketX[ip] = rocketX[ip] + 2;
     }
-    for(ip=0; ip<3; ip=ip + 1) {
+    for(var ip=0; ip<10; ip=ip + 1) {
         // nothing here for now
 
     }
@@ -30,7 +36,7 @@ function draw() {
     // Tuk naprogramirai kakvo da se risuva
     drawImage(backField, 0, 0, 800, 600);
     drawImage(femaleAction, myX, myY, 60, 80);
-    for(ip=0; ip<3; ip=ip+1){
+    for(var ip=0; ip<10; ip=ip+1){
         drawImage(rocket[0], rocketX[ip], rocketY[ip], 35, 60);
     }
 }
